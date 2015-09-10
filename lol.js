c12633ce41210717078d4efd3b5cbb8d0280f277
hesus001 <adamoctav@gmail.com>
2015-09-10 14:02:23 +0300

    link

---
+++ lol.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Our hawaiian greeting is displayed as soon as the page loads,
+
+    console.log('Aloha');
+var lol = document.getElementsByTagName('h1');
+	setTimeout(function(){
+
+	lol.style.color = "green";
+	},4000)
+});
